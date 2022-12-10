52767f5de06143ab98f539e1f7feb497b9be4e66
Paul Bartl <paul.langt@gmail.com>
2022-12-10 10:00:29 +0100

    simple tests

--- app/src/test/java/com/dcelevator/app/AppTest.java
+++ app/src/test/java/com/dcelevator/app/AppTest.java
@@ -1,8 +1,14 @@
 package com.dcelevator.app;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import com.dcelevator.app.models.Direction;
+import com.dcelevator.app.models.Elevator;
+import com.dcelevator.app.models.ElevatorState;
+import com.dcelevator.app.models.Request;
 
 /**
  * Unit test for simple App.
@@ -17,4 +23,35 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void testElevatorSystem() {
+        ElevatorSystem test = new ElevatorSystem();
+        
+        Elevator e0 = new Elevator("0");
+        e0.setState(ElevatorState.INUSE);
+        Elevator e1 = new Elevator("1");
+        e1.setState(ElevatorState.IDLE);
+
+        Thread t0 = new Thread(e0);
+        Thread t1 = new Thread(e1);
+        t0.start();
+        t1.start();
+
+        test.addElevator(e0);
+        test.addElevator(e1);
+
+        assertEquals(e1, test.getIdleElevator());
+    }
+
+    @Test
+    public void testRequestDirection() {
+        Request requestUp = new Request("0", 1, 3);
+        Request requestDown = new Request("1", 3, 1);        
+        assertEquals(Direction.UP, requestUp.getDirection());
+        assertEquals(Direction.DOWN, requestDown.getDirection());
+    }
 }
+
+
+
