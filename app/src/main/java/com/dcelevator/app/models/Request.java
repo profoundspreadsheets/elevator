2b6ceb2f72411f43d175c15cea9409050be369c9
Paul Bartl <paul.langt@gmail.com>
2022-12-10 10:00:20 +0100

    request holds direction

--- app/src/main/java/com/dcelevator/app/models/Request.java
+++ app/src/main/java/com/dcelevator/app/models/Request.java
@@ -2,23 +2,36 @@ package com.dcelevator.app.models;
 
 public class Request {
     @SuppressWarnings("unused")
-    private String id;
-    private int currentFloor; // from which floor was request entered
-    private int destinationFloor;
+    private final String id;
+    private final int currentFloor; // from which floor was request entered
+    private final int destinationFloor;
+    private final Direction direction;
 
+    public Request (String id, int currentFloor, int destinationFloor) {
+        this.id = id;
+        this.currentFloor = currentFloor;
+        this.destinationFloor = destinationFloor;
+        if (currentFloor > destinationFloor) {
+            this.direction = Direction.DOWN;
+        }
+        else {
+            this.direction = Direction.UP;
+        }
+    }
+    
     public int getCurrentFloor() {
         return currentFloor;
-    }
-
-    public void setCurrentFloor(int currentFloor) {
-        this.currentFloor = currentFloor;
     }
 
     public int getDestinationFloor() {
         return destinationFloor;
     }
 
-    public void setDestinationFloor(int destinationFloor) {
-        this.destinationFloor = destinationFloor;
+    public String getId() {
+        return id;
+    }
+
+    public Direction getDirection() {
+        return direction;
     }
 }
