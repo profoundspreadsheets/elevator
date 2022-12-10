667ccf809b02b7906591b43559688229e856e7f5
Paul Bartl <paul.langt@gmail.com>
2022-12-10 11:40:16 +0100

    logic for gettings closest moving or idle elevator

--- app/src/main/java/com/dcelevator/app/ElevatorSystem.java
+++ app/src/main/java/com/dcelevator/app/ElevatorSystem.java
@@ -1,6 +1,7 @@
 package com.dcelevator.app;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import com.dcelevator.app.models.Elevator;
@@ -23,22 +24,22 @@ public class ElevatorSystem {
     }
 
     public void addRequest(Request request) {
-        
+
     }
 
-    public Elevator getMovingAbleToPickUpElevator(Request request) {
-        for (Elevator elevator : elevators) {
-            if (elevator.getState() == ElevatorState.INUSE) {
-                
-                
-                
-                return elevator;
-            }
-        }
-        return null;
+    public Elevator getClosestMovingElevator(Request request) {
+        return elevators.stream()
+                .filter(elevator -> elevator.getState() == ElevatorState.INUSE)
+                .filter(elevator -> elevator.getDirection() == request.getDirection())
+                .min(Comparator
+                        .comparingInt(elevator -> Math.abs(elevator.getCurrentFloor() - request.getCurrentFloor())))
+                .orElse(null);
     }
-    
-    public Elevator getIdleElevator() {
-        return elevators.stream().filter(x -> x.getState() == ElevatorState.IDLE).findFirst().get();
+
+    public Elevator getClosestIdleElevator(Request request) {
+        return elevators.stream().filter(elevator -> elevator.getState() == ElevatorState.IDLE)
+                .min(Comparator
+                        .comparingInt(elevator -> Math.abs(elevator.getCurrentFloor() - request.getCurrentFloor())))
+                .orElse(null);
     }
 }
