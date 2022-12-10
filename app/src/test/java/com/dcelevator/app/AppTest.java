076df956907f0b1009c12641e2f6c006e8c7db81
Paul Bartl <paul.langt@gmail.com>
2022-12-10 11:40:55 +0100

    remove unused import

--- app/src/test/java/com/dcelevator/app/AppTest.java
+++ app/src/test/java/com/dcelevator/app/AppTest.java
@@ -3,8 +3,6 @@ package com.dcelevator.app;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.NoSuchElementException;
-
 import org.junit.Test;
 
 import com.dcelevator.app.models.Direction;
